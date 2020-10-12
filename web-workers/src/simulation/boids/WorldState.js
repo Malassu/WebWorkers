8c66d8c329397253bae183d08883830b7f95ac94
lari alakukku <lari.alakukku@aalto.fi>
2020-10-12 16:39:21 +0300

    Added files and world state definition

---
+++ web-workers/src/simulation/boids/WorldState.js
@@ -0,0 +1,29 @@
+class WorldState {
+  constructor(options) {
+
+    const defaultState = {
+      bounded: true,     // The worlds should have "walls" that boids bounce off of.
+      maxSpeed: 1.0,     // Max speed all boids should follow.
+      collision: true,  // Boids can bounce off of each other.
+      explosion: true   // Boids can randomly "explode" pushing surrounding boids away. 
+    };
+
+    Object.keys(defaultState).forEach(key => this[key] = (typeof options[key] === defaultState[key] ) ? options[key] : defaultState[key]);
+    
+  };
+
+  setState(option, value) {
+    if (this[option !== undefined])
+      this[option] = value;
+  };
+
+  getState(option) {
+    if (this[option !== undefined])
+      return this[option];
+
+    return Error("Value not defined");
+  };
+
+};
+
+export default WorldState;
